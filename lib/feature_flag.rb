695cf4149ddae3daa7e33b0cb883a13fe551d094
trevor-oke <trevor.oke@mosaic.com>
2013-10-15 18:03:03 -0400

    wip: interim work

--- lib/feature_flag.rb
+++ lib/feature_flag.rb
@@ -1,5 +1,15 @@
 require "feature_flag/version"
 
 module FeatureFlag
-  # Your code goes here...
+  class Toggle
+#    myfilter = -> { false }
+#
+#    FeatureFlag::Toggle.feature(:myfeature) do
+#      # some work
+#    end
+#
+#    FeatureFlag::Toggle.feature(:yourfeature, myfilter) do
+#      #work
+#    end
+  end
 end
--- spec/lib/feature_flag_spec.rb
+++ spec/lib/feature_flag_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe FeatureFlag do
-  it "defines tests and such"
+describe FeatureFlag::Toggle do
+  describe "#feature" do
+    context "when the feature is false" do
+
+    end
+    # when the feature is null
+    # when the feature is false
+    # when the feature is true
+    # when the filter is not present
+    # when the filter is true
+    # when the filter is false
+
+    it "defines tests and such"
+  end
 end
